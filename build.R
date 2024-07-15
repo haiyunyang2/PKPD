73f12cdb24a37036b8450a74c502badb7767b205
haiyunyang <147833108+haiyunyang@users.noreply.github.com>
2024-07-15 20:48:57 +0800

    AAA

---
+++ build.R
@@ -0,0 +1,10 @@
+
+
+## Windows for xiaoming
+devtools::document(roclets = c('rd', 'collate', 'namespace'))
+devtools::build(path='D:\\GBDR\\easyGBDR',binary = T)
+
+getwd()
+## MAC
+devtools::document(roclets = c('rd', 'collate', 'namespace'))
+devtools::build(path='/Users/liyuancun/Desktop/easy/easyFEARS',binary = T)
